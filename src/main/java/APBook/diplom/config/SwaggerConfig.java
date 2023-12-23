36397ba79c3653f3d5fef52fee2b25a854fb3b69
railr <railra498@gmail.com>
2023-12-23 17:13:32 +0300

    Обработка исключений

--- src/main/java/APBook/diplom/config/SwaggerConfig.java
+++ src/main/java/APBook/diplom/config/SwaggerConfig.java
@@ -1,11 +1,8 @@
 package APBook.diplom.config;
 
 import org.springframework.context.annotation.*;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,7 +10,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
     @Bean
     public Docket SwaggerApi() {
         return new Docket(DocumentationType.SWAGGER_2)
--- src/main/java/APBook/diplom/contollers/ProjectController.java
+++ src/main/java/APBook/diplom/contollers/ProjectController.java
@@ -5,6 +5,8 @@ import APBook.diplom.service.ProjectService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.Tag;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,14 +15,13 @@ import java.util.List;
 @RestController
 @CrossOrigin
 @RequestMapping("/api/project")
-@Api(tags = "Проекты", description = "Контроллер для управления проектами")
+@Api(tags = "Проекты")
 public class ProjectController {
     private final ProjectService projectService;
 
     public ProjectController(ProjectService projectService) {
         this.projectService = projectService;
     }
-
 
     @GetMapping()
     @ApiOperation(value = "Получение всех проектов")
@@ -30,25 +31,43 @@ public class ProjectController {
 
     @GetMapping("/{id}")
     @ApiOperation(value = "Получение одного проекта")
-    public Project show(@PathVariable long id){
-        return projectService.get(id);
+    public ResponseEntity<Project> show(@PathVariable long id){
+        try {
+            return new ResponseEntity(projectService.get(id), HttpStatus.OK);
+        } catch (Exception exception){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
     }
 
     @PostMapping
     @ApiOperation(value = "Добавление проекта")
-    public void add(@RequestBody Project project){
+    public ResponseEntity<String> add(@RequestBody Project project){
         projectService.add(project);
+        String createdObject = "Проект успешно создан";
+        return new ResponseEntity<>(createdObject, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     @ApiOperation(value = "Изменение проекта")
-    public void update(@PathVariable long id, @RequestBody Project project){
-        projectService.update(id, project);
+    public ResponseEntity<String> update(@PathVariable long id, @RequestBody Project project){
+        if(project.getId().equals(id)){
+            projectService.update(id, project);
+            return new ResponseEntity<>("Проект изменен", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Номера не совпадают", HttpStatus.BAD_REQUEST);
     }
 
     @DeleteMapping("/{id}")
     @ApiOperation(value="Удаление проекта")
-    public void delete(@PathVariable long id){
-        projectService.delete(id);
+    public ResponseEntity<String> delete(@PathVariable long id){
+        try{
+            projectService.delete(id);
+            return new ResponseEntity<>("Проект удален", HttpStatus.OK);
+        } catch (EmptyResultDataAccessException ex){
+            return new ResponseEntity<>("Проект не найден", HttpStatus.NOT_FOUND);
+        }
+
+
     }
 }
